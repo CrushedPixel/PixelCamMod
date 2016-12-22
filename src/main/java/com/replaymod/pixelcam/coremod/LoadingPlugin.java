fac609fbd0bc74eab7ab7ee3042fcd6aa7bfd4d3
misson20000 <xenotoad@xenotoad.net>
2016-12-22 01:43:09 -0800

    Update to 1.11, fix #16

--- src/main/java/com/replaymod/pixelcam/coremod/LoadingPlugin.java
+++ src/main/java/com/replaymod/pixelcam/coremod/LoadingPlugin.java
@@ -22,7 +22,7 @@ import net.minecraftforge.fml.relauncher.IFMLLoadingPlugin;
 
 import java.util.Map;
 
-@IFMLLoadingPlugin.MCVersion("1.10.2")
+@IFMLLoadingPlugin.MCVersion("1.11")
 @IFMLLoadingPlugin.Name("PixelCamTilt")
 public class LoadingPlugin implements IFMLLoadingPlugin {
 
